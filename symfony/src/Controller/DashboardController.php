57a11e1ee6f3f64a0fe65c3c72083638344e63a1
Andre Baldwin <andrebaldwin@Andres-MacBook-Pro.local>
2026-08-06 20:38:17 -0400

    first commit

---
+++ symfony/src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'dashboard')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
