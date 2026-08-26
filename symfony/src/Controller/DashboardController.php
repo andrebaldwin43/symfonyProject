46df312f8ed2b7b7600cf7101b09fbda6d94a6c9
Andre Baldwin <andrebaldwin@Andres-MacBook-Pro.local>
2026-08-26 19:01:18 -0400

    connect to google api

--- symfony/src/Controller/DashboardController.php
+++ symfony/src/Controller/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\GoogleAPIService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractController
@@ -12,5 +14,16 @@ class DashboardController extends AbstractController
     public function index(): Response
     {
         return $this->render('dashboard.html.twig', []);
+    }
+
+    #[Route('/weather', name: 'app_weather_api')]
+    public function weatherAPI(GoogleAPIService $weatherService): JsonResponse
+    {
+        try {
+            $data = $weatherService->getCurrentConditions('48186');
+            return $this->json($data);
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
