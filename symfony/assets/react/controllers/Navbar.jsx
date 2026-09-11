0bef7d9df575a57abc481907d266dcc807d6d083
Andre Baldwin <andrebaldwin@Andres-MacBook-Pro.local>
2026-09-10 20:09:31 -0400

    move some things around

--- symfony/assets/react/controllers/Navbar.jsx
+++ symfony/assets/react/controllers/Navbar.jsx
@@ -1,21 +1,19 @@
-import styles from '../../styles/app.css';
 import { useState, React } from 'react';
 
-export default function Navbar() {
+export default function Navbar({ currentTheme, onToggleTheme }) {
     const [open, setOpen] = useState(false);
-    const [theme, setTheme] = useState('light');
-    
+
     return (
         <nav>
             <div className="flex items-center justify-between bg-slate-900 text-white dark:bg-slate-900 dark:text-white p-4">
-                <div className='font-bold'>Logo</div>
+                <div className='font-bold'>Welcome!</div>
 
                 <div className='hidden sm:flex gap-2'>
                     <span className='cursor-pointer'>Home</span>
                     {/* <Link to="/about">Go to About Page</Link> */}
                     <span className='cursor-pointer'>Contact</span>
-                    <button className='text-xl cursor-pointer' onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-                        {theme === 'light' ? '☀️' : '🌙'}
+                    <button className='text-xl cursor-pointer' onClick={() => onToggleTheme(currentTheme === 'light' ? 'dark' : 'light')}>
+                        {currentTheme === 'light' ? '☀️' : '🌙'}
                     </button>
                 </div>
 
@@ -30,8 +28,8 @@ export default function Navbar() {
                     <span>Home</span>
                     <span>About</span>
                     <span>Contact</span>
-                    <button className='text-xl cursor-pointer sm:hidden' onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-                        {theme === 'light' ? '☀️' : '🌙'}
+                    <button className='text-xl cursor-pointer sm:hidden' onClick={() => onToggleTheme(currentTheme === 'light' ? 'dark' : 'light')}>
+                        {currentTheme === 'light' ? '☀️' : '🌙'}
                     </button>
                 </div>
             )}
